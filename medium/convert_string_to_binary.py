5b349cba0fe9b7ec05efe47dafbb2a1839dcd1d8
kemechedzhiev <kemechedzhiev@gmail.com>
2020-12-29 17:05:38 +0300

    Exercise category: medium Name: Convert string to binary Status: in progress

---
+++ medium/convert_string_to_binary.py
@@ -0,0 +1,10 @@
+import sys
+
+# TODO Your input variable will contain a string of three lowercase letters.
+#  From this, you'll need to convert it to binary and
+#  print out the 24 number representation of those letters in binary.
+
+
+if __name__ == '__main__':
+    print("Mission started!")
+    str_to_convert = sys.argv[1]
