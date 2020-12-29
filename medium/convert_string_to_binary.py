35a2c43979c26c54214382590905731423bf85ec
kemechedzhiev <kemechedzhiev@gmail.com>
2020-12-29 17:25:12 +0300

    Exercise category: medium Name: Convert string to binary Status: solved

--- medium/convert_string_to_binary.py
+++ medium/convert_string_to_binary.py
@@ -1,10 +1,22 @@
 import sys
 
+
 # TODO Your input variable will contain a string of three lowercase letters.
 #  From this, you'll need to convert it to binary and
 #  print out the 24 number representation of those letters in binary.
+
+def convert_to_binary(raw_text):
+    binary = ''
+    for i in raw_text:
+        prefix = ''
+        letter_binary = format(ord(i), 'b')
+        if len(letter_binary) < 8:
+            prefix = '0' * (8 - len(letter_binary))
+        binary += prefix + str(letter_binary)
+    return binary
 
 
 if __name__ == '__main__':
     print("Mission started!")
     str_to_convert = sys.argv[1]
+    print(convert_to_binary(str_to_convert))
