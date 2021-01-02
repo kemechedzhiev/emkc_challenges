011888eafaa36bb7b92448baeb44f34b56899532
kemechedzhiev <kemechedzhiev@gmail.com>
2021-01-02 23:33:06 +0300

    Exercise category: medium Name: Print the fibonacci sequence Status: in_progress

--- medium/fibonacci_numbers.py
+++ medium/fibonacci_numbers.py
@@ -1,29 +1,32 @@
 import sys
+import math
 
 
 # TODO Your input variable will contain a fibonacci sequence number. Your job is to calculate the next n digits.
 
 def find_the_fibonacci_by_index(index):
     fib1 = fib2 = 1
-    for i in range(2, int(index) + 1):
+    if index <= 2:
+        return fib1
+    for i in range(2, int(index)):
         fib1, fib2 = fib2, fib1 + fib2
     return fib2
 
 
-def find_the_fibonacci_sequence_from_index(index, seq_length):
-    int_index = int(index)
-    result = list()
-    answer = ''
-    for i in range(int(seq_length)):
-        result.append(find_the_fibonacci_by_index(int_index))
-        int_index += 1
-    for j in result:
-        answer += str(j) + ','
-    print(answer[:-1])
+def find_index_of_the_fibonacci_number(given_number):
+    return round(2.078087 + math.log(int(given_number)) + 1.672276) + 2
+
+
+def find_the_fibonacci_sequence_starting_from(starting_num, seq_length):
+    result = ""
+    starting_index = int(find_index_of_the_fibonacci_number(starting_num)) + 1
+    for i in range(starting_index, starting_index + int(seq_length)):
+        result += str(find_the_fibonacci_by_index(i)) + ","
+    print(result[:-1])
 
 
 if __name__ == '__main__':
     print("Mission started!")
     val1 = sys.argv[1]
     val2 = sys.argv[2]
-    find_the_fibonacci_sequence_from_index(val1, val2)
+    find_the_fibonacci_sequence_starting_from(val1, val2)
